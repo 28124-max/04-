c6f2c2c0b2efb3587f792de5b0bc7052c9319afc
28124-max <28124@ud.ac.th>
2026-09-17 15:24:04 +0700

    Update Examination_1.py

--- Examination_1.py
+++ Examination_1.py
@@ -6,6 +6,9 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-s1 = int(input())
-s2 = int(input())
-s3 = int(input())
+height = int(input("ความสูงซม"))
+if height>=140
+ print("Can Ride")
+else:
+if height<140
+ print("Cannot Ride")
